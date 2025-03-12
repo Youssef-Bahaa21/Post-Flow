6aa08df78b61b0e6355d7663e1f825cbc79a1037
Youssef-Bahaa21 <youssefbahaa211@gmail.com>
2025-03-12 02:23:26 +0200

    Post-Flow

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '  post/:id',
+    path: 'post/:id',
     renderMode: RenderMode.Client
   },
   {
